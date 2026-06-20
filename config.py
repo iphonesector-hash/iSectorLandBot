39b882b41aa51fee56dacad305207f58817f0439
iphonesector-hash <iphone.sector@gmail.com>
2026-06-20 19:28:05 +0330

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,3 @@
-import os
-
-TOKEN = os.getenv("8804544118:AAFdqHyr6EJn-xYKBKE0-vYkzTWRpyVil1M")
-
-if TOKEN:
-    TOKEN = TOKEN.strip()
+TOKEN = "8804544118:AAFdqHyr6EJn-xYKBKE0-vYkzTWRpyVil1M"
 
 BOT_NAME = "🌻 iSectorLand Bot 🤖"
