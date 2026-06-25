e7bce842e76e6c7de24e4ab25b0ffd595f4e7f62
iphonesector-hash <iphone.sector@gmail.com>
2026-06-25 21:27:55 +0330

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,4 @@
-# ⚙️ تنظیمات ربات SectorLand
-# ================================
-
 TOKEN = "8804544118:AAGotcjB1GRe4BDdxATEYCKyKFV79fd0s_g"
 BOT_NAME = "🌻 iSectorLand Bot 🤖"
+SUPPORT_USERNAME = "sector_ad"
 OWNER_ID = 5147526780
-SUPPORT_ID = 7079347270  # ایدی پشتیبانی سکتورلند
