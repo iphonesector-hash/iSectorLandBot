78bec5729c3c6f382ce873a93bb63e2ac42466fe
iphonesector-hash <iphone.sector@gmail.com>
2026-06-25 21:19:39 +0330

    Update warnings.py

--- modules/warnings.py
+++ modules/warnings.py
@@ -7,21 +7,21 @@ FILE = "warnings.json"
 def load():
     if not os.path.exists(FILE):
         return {}
-    with open(FILE, "r") as f:
+    with open(FILE, "r", encoding="utf-8") as f:
         return json.load(f)
 
 
 def save(data):
-    with open(FILE, "w") as f:
-        json.dump(data, f, indent=4)
+    with open(FILE, "w", encoding="utf-8") as f:
+        json.dump(data, f, ensure_ascii=False, indent=4)
 
 
-def get_warn(user_id):
+def get_warn(user_id: int) -> int:
     data = load()
     return data.get(str(user_id), 0)
 
 
-def add_warn(user_id):
+def add_warn(user_id: int) -> int:
     data = load()
     uid = str(user_id)
     data[uid] = data.get(uid, 0) + 1
@@ -29,7 +29,7 @@ def add_warn(user_id):
     return data[uid]
 
 
-def clear_warn(user_id):
+def clear_warn(user_id: int):
     data = load()
     uid = str(user_id)
     if uid in data:
